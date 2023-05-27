fadf3ce43a988b9d398529f5b58f6ad6004077b1
mohmad almmidani <mohmad>
2023-05-27 19:33:29 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 function App() {
   return (
     <>
-      <h1 className="name">mohmad alastora </h1>
-      <h2 className="na">man nga</h2>
-    </>
+    <header className="head">
+      <h1 className="title">mohamad</h1>
+      <div className="a">
+        <a href="">JS</a>
+        <a href="">Html</a>
+        <a href="">Css</a>
+      </div>
+    </header>
+    <main>
+      <h1 className="main">home</h1>
+    </main>
+    <footer>
+      <h5>make by mohmad-alastora</h5>
+    </footer>
+  </>
+  
   );
 }
 
